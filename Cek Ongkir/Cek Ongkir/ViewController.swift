2d45264f2dfee185a660095a541e7e82e1e7768d
julis <kangjulis@gmail.com>
2017-02-23 23:26:01 +0700

    project setup
    project setup with swift 2.2

---
+++ Cek Ongkir/Cek Ongkir/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cek Ongkir
+//
+//  Created by julis on 2/23/17.
+//  Copyright © 2017 julis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
