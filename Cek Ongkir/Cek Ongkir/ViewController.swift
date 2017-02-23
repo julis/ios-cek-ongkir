2b639a9aad1b806f95823ff59924674535005396
julis <kangjulis@gmail.com>
2017-02-24 00:45:14 +0700

    learn consume api
    learn to consume REST API using swift 2.2

--- Cek Ongkir/Cek Ongkir/ViewController.swift
+++ Cek Ongkir/Cek Ongkir/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textViewResult: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,55 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didTappedButtonGetProvince(sender: UIButton) {
+        let headers = [Key.apiKeyName: Key.apiKeyValue, Key.apiIOSKeyName: Key.apiIOSKeyValue]
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: OngkirAPI.provinceURL)!,
+                                          cachePolicy: .UseProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.HTTPMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = NSURLSession.sharedSession()
+        let dataTask = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? NSHTTPURLResponse
+                print(httpResponse)
+                
+                
+                
+                
+                if let data = data,
+                let json = try? NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary {
+                    print(json)
+                    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)){
+                        dispatch_async(dispatch_get_main_queue()){
+                            self.textViewResult.text = "\(httpResponse) \n \(json)"
+
+                        }
+                    }
+                }
+                
+                
+
+                
+            }
+        })
+        
+        dataTask.resume()
+        
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func didTappedButtonGetCity(sender: AnyObject) {
     }
 
 
