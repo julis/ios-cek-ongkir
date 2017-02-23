c10ac6ebd9d8423110ab56ef50cddd7f9019dd41
julis <kangjulis@gmail.com>
2017-02-24 00:49:57 +0700

    add get city data

--- Cek Ongkir/Cek Ongkir/ViewController.swift
+++ Cek Ongkir/Cek Ongkir/ViewController.swift
@@ -69,6 +69,44 @@ class ViewController: UIViewController {
     
     
     @IBAction func didTappedButtonGetCity(sender: AnyObject) {
+        
+        let headers = [Key.apiKeyName: Key.apiKeyValue, Key.apiIOSKeyName: Key.apiIOSKeyValue]
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: OngkirAPI.cityURL)!,
+                                          cachePolicy: .UseProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.HTTPMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = NSURLSession.sharedSession()
+        let dataTask = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? NSHTTPURLResponse
+                print(httpResponse)
+                
+                
+                
+                
+                if let data = data,
+                    let json = try? NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary {
+                    print(json)
+                    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)){
+                        dispatch_async(dispatch_get_main_queue()){
+                            self.textViewResult.text = "\(httpResponse) \n \(json)"
+                            
+                        }
+                    }
+                }
+                
+                
+                
+                
+            }
+        })
+        
+        dataTask.resume()
     }
 
 
